dc790dbfd662925f94a338a671e8649c04a0eb2d
Jean-Philippe Dupéré <jpdupere@gmail.com>
2022-02-08 15:03:59 -0500

    refactored code into shopify_api, enforce a request limit of 2 per second, finished writing to db

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
 'use strict';
-const fetch = require('node-fetch');
-const api = require('./secret.json');
+
 const db = require('./db.js');
-const { addRecord } = require('./csv');
-const baseUrl = `https://${api.key}:${api.password}@`;
-const fields = [
-    'id',
-    'contact_email',
-    'order_number',
-    'phone',
-    'customer',
-    'line_items',
-]
-const apiUrl = 'taygra-shoes.myshopify.com/admin/api/2022-01/'; 
-const brazilLocationId = 63141773507;
-
-
-const buildOrderUrl = () => {
-    return baseUrl + apiUrl + 'orders.json?fields=' + fields.join(',');
-}
-
-const buildFulfillmentOrderUrl = (orderId) => {
-    return baseUrl + apiUrl + `orders/${orderId}/fulfillment_orders.json`;
-}
-
-const buildVariantUrl = (variantId) => {
-    return baseUrl + apiUrl + `variants/${variantId}.json`;
-}
-
-const getImageSrc = async (product_id, image_id) => {
-    let src;
-    if (image_id) {
-        const url = baseUrl + apiUrl + `products/${product_id}/images/${image_id}.json`;
-        const response = await fetch(url);
-        const {image} = await response.json();
-        src = image.src;
-    } else {
-        const url = baseUrl + apiUrl + `products/${product_id}/images.json`;
-        const response = await fetch(url);
-        const {images} = await response.json();
-        src = images[0].src;
-    }
-    return src;
-}
-
-const getNextUrl = (links = []) => {
-    const next = links.find((value) => value.match(/<(.+)>; rel="next"/));
-    return next ? next.match(/<https:\/\/(.+)>; rel="next"/)[1] : null;
-}
-
-const getBrazilLineItems = async (orderId) => {
-    const response = await fetch(buildFulfillmentOrderUrl(orderId));
-    const { fulfillment_orders } = await response.json();
-    let brazilLineItems = {};
-    for (const fo of fulfillment_orders) {
-        if (fo.status === 'open' && fo.assigned_location_id === brazilLocationId) {
-            for (const li of fo.line_items) {
-                brazilLineItems[li.line_item_id] = true;
-            }
-        }
-    }
-    return brazilLineItems;
-}
+const { getOrderList, getBrazilLineItems, getVariantImgSrc } = require('./shopify_api.js');
 
 const extractData = async (orderList) => {
     const lineItems = [];
     for (const order of orderList) {
+        console.log(`Extracting data for order ${order.order_number}`);
         // get a map of lineItems which locations are in Brazil
         const brazilLineItems = await getBrazilLineItems(order.id);
         for (const lineItem of order.line_items) {
@@ -89,55 +30,48 @@ const extractData = async (orderList) => {
     return lineItems;
 }
 
-const getPageOrders = async (url) => {
-    const response = await fetch(url);
-    const { orders } = await response.json();
-    const nextUrl = getNextUrl(response.headers.raw()['link']);
-    return { pageOrders: orders, nextUrl };
-}
-
-const loopThroughPages = async () => {
-    let orderList = [];
-    let url = buildOrderUrl();
-    let currentPage = 1;
-    do {
-        const { pageOrders, nextUrl } = await getPageOrders(url);
-        console.log(`Found ${pageOrders.length} orders in page ${currentPage}`);
-        orderList.push(...pageOrders);
-        url = nextUrl ? baseUrl + nextUrl : null;
-        currentPage++;
-    } while (url);
-    return orderList;
-}
-
-const getVariantImgSrc = async (variantId) => {
-    const variantUrl = buildVariantUrl(variantId);
-    const variantResponse = await fetch(variantUrl);
-    const {variant} = await variantResponse.json();
-    const imageSrc = getImageSrc(variant.product_id, variant.image_id);
-    return imageSrc;
-}
-
 const updateDb = async () => {
-    const orderList = await loopThroughPages();
+    const orderList = await getOrderList();
     const lineItems = await extractData(orderList);
     console.log(`Unfulfilled Shopify line-item count: ${lineItems.length}`);
     const localDbState = db.read();
-    const newLocalDb = {};
     // create new localDb state with all the existing lineItems
+    const newDbState = {};
     for (const lineItem of lineItems) {
-        const lineItemUid = `${lineItem.number}|${lineItem.sku}`;
-        let imgSrc;
+        const lineItemUid = `${lineItem.number}|${lineItem.sku}`
         if (!localDbState[lineItemUid]) {
             // line-item doesn't exist in localDb
             // fetch the product image url
-            imgSrc = await getVariantImgSrc(lineItem.variantId);
+            lineItem.imgSrc = await getVariantImgSrc(lineItem.variantId);
+            console.log(`Fetched image source for line-item ${lineItemUid}: ${lineItem.imgSrc}`);
+            newDbState[lineItemUid] = {
+                ...lineItem,
+                reservedQty: 0,
+                sentQty: 0
+            };
         } else {
-            imgSrc = localDbState[lineItemUid].imgSrc;
+            newDbState[lineItemUid] = {
+                ...lineItem,
+                imgSrc: localDbState[lineItemUid].imgSrc,
+                reservedQty: localDbState[lineItemUid].reservedQty,
+                sentQty: localDbState[lineItemUid].sentQty
+            };
         }
-        newLocalDb[lineItemUid] = {...lineItem, imgSrc};
     }
+    console.log(`${lineItems.length} line-items to fulfill from Brazil`);
+
     // add the lineItems from the original localDb that don't exist in the new db but have reservedQty
+    for (const lineItemUid in localDbState) {
+        if (!lineItems[lineItemUid] && localDbState[lineItemUid].reservedQty > 0) {
+            // If there is a reserved quatity for an item that is no longer unfulfilled (ex: cancelled or fulfilled), keep that item in the list.
+            console.log(`Line-item ${lineItemUid} has a reserved qty of ${localDbState[lineItemUid].reservedQty} but is no longer needed.`)
+            newDbState[lineItemUid] = localDbState[lineItemUid];
+        }
+    }
+
+    // override the db with the new line-items
+    db.write(newDbState);
+    console.log('Local database updated');
 }
 
 (async () => {
---
+++ shopify_api.js
@@ -0,0 +1,101 @@
+const fetch = require('node-fetch');
+const api = require('./secret.json');
+// Rate limit: 2 requests per second (500 ms)
+const delay = 500;
+let lastRequestTimestamp = 0;
+const baseUrl = `https://${api.key}:${api.password}@`;
+const apiUrl = 'taygra-shoes.myshopify.com/admin/api/2022-01/'; 
+const brazilLocationId = 63141773507;
+
+const request = async (url) => {
+    // Make sure only a maximum of 2 requests per seconds are sent
+    while (Date.now() - lastRequestTimestamp < delay) { /* do nothing */ }
+    // Proceed with request
+    const response = await fetch(url);
+    lastRequestTimestamp = Date.now();
+    return response;
+}
+
+const getNextUrl = (links = []) => {
+    const next = links.find((value) => value.match(/<(.+)>; rel="next"/));
+    return next ? next.match(/<https:\/\/(.+)>; rel="next"/)[1] : null;
+}
+
+const getPageOrders = async (url) => {
+    const response = await request(url);
+    const { orders } = await response.json();
+    const nextUrl = getNextUrl(response.headers.raw()['link']);
+    return { pageOrders: orders, nextUrl };
+}
+
+/**
+ * Get an array of all the unfulfilled orders from Shopify
+ * @returns an array of Shopify's Order object
+ */
+const getOrderList = async () => {
+    let orderList = [];
+    let url = baseUrl + apiUrl + 'orders.json?fulfillment_status=unfulfilled&fields=id,contact_email,order_number,phone,customer,line_items';
+    let currentPage = 1;
+    do {
+        const { pageOrders, nextUrl } = await getPageOrders(url);
+        console.log(`Found ${pageOrders.length} orders in page ${currentPage}`);
+        orderList.push(...pageOrders);
+        url = nextUrl ? baseUrl + nextUrl : null;
+        currentPage++;
+    } while (url);
+    return orderList;
+}
+
+/**
+ * For a given order, get the line items that need to be fulfilled from Brazil.
+ * @param {string} orderId The id of the Shopify Order object
+ * @returns an array of the line-items from the Shopify's Fulfillement Order object.
+ */
+const getBrazilLineItems = async (orderId) => {
+    const url = baseUrl + apiUrl + `orders/${orderId}/fulfillment_orders.json`;
+    const response = await request(url);
+    const { fulfillment_orders } = await response.json();
+    let brazilLineItems = {};
+    for (const fo of fulfillment_orders) {
+        if (fo.status === 'open' && fo.assigned_location_id === brazilLocationId) {
+            for (const li of fo.line_items) {
+                brazilLineItems[li.line_item_id] = true;
+            }
+        }
+    }
+    return brazilLineItems;
+}
+
+const getImageSrc = async (product_id, image_id) => {
+    let src;
+    if (image_id) {
+        const url = baseUrl + apiUrl + `products/${product_id}/images/${image_id}.json`;
+        const response = await request(url);
+        const {image} = await response.json();
+        src = image.src;
+    } else {
+        const url = baseUrl + apiUrl + `products/${product_id}/images.json`;
+        const response = await request(url);
+        const {images} = await response.json();
+        src = images.sort((a,b) => {
+            return a.position - b.position;
+        })[0].src;
+    }
+    return src;
+}
+
+/**
+ * Find an image url from a variant id
+ * @param {string} variantId 
+ * @returns the url of the variant image if the variant has an image, or the url of the product image in position number 1
+ */
+const getVariantImgSrc = async (variantId) => {
+    const variantUrl = baseUrl + apiUrl + `variants/${variantId}.json?fields=product_id,image_id`;
+    const variantResponse = await request(variantUrl);
+    const {variant} = await variantResponse.json();
+    if (!variant) console.log(variantResponse);
+    const imageSrc = getImageSrc(variant.product_id, variant.image_id);
+    return imageSrc;
+}
+
+module.exports = { getOrderList, getBrazilLineItems, getVariantImgSrc };
